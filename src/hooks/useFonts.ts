790c3d4a1d0f1af04c43ab8a7398704cb8ae2672
theaarc <109694966+theaarc@users.noreply.github.com>
2025-11-25 04:14:43 +0100

    feat: Add Satoshi and Urbanist font files and update font loading hook to include new font paths

--- src/hooks/useFonts.ts
+++ src/hooks/useFonts.ts
@@ -1,32 +1,17 @@
-/**
- * Custom Hook for Loading Fonts
- * Loads Urbanist and Satoshi Variable fonts
- * 
- * NOTE: Font files must be added to assets/fonts/ directory
- * - Urbanist-Regular.ttf, Urbanist-Medium.ttf, Urbanist-SemiBold.ttf, Urbanist-Bold.ttf
- * - Satoshi-Regular.otf, Satoshi-Medium.otf, Satoshi-Bold.otf
- * 
- * For now, uses system fonts as fallback until custom fonts are added
- * When font files are added, uncomment the useExpoFonts code below
- */
 
 import { useFonts as useExpoFonts } from 'expo-font';
 
 export const useFonts = () => {
-  // TODO: Uncomment when font files are added to assets/fonts/
-  // const [fontsLoaded, fontError] = useExpoFonts({
-  //   'Urbanist-Regular': require('../../assets/fonts/Urbanist-Regular.ttf'),
-  //   'Urbanist-Medium': require('../../assets/fonts/Urbanist-Medium.ttf'),
-  //   'Urbanist-SemiBold': require('../../assets/fonts/Urbanist-SemiBold.ttf'),
-  //   'Urbanist-Bold': require('../../assets/fonts/Urbanist-Bold.ttf'),
-  //   'Satoshi-Regular': require('../../assets/fonts/Satoshi-Regular.otf'),
-  //   'Satoshi-Medium': require('../../assets/fonts/Satoshi-Medium.otf'),
-  //   'Satoshi-Bold': require('../../assets/fonts/Satoshi-Bold.otf'),
-  // });
+  const [fontsLoaded, fontError] = useExpoFonts({
+    'Urbanist-Regular': require('../../assets/fonts/Urbanist/Urbanist-Regular.ttf'),
+    'Urbanist-Medium': require('../../assets/fonts/Urbanist/Urbanist-Medium.ttf'),
+    'Urbanist-SemiBold': require('../../assets/fonts/Urbanist/Urbanist-SemiBold.ttf'),
+    'Urbanist-Bold': require('../../assets/fonts/Urbanist/Urbanist-Bold.ttf'),
+    'Satoshi-Regular': require('../../assets/fonts/Satoshi Variable/Satoshi-Regular.otf'),
+    'Satoshi-Medium': require('../../assets/fonts/Satoshi Variable/Satoshi-Medium.otf'),
+    'Satoshi-Bold': require('../../assets/fonts/Satoshi Variable/Satoshi-Bold.otf'),
+  });
 
-  // Temporary: Return true to allow app to load with system fonts
-  // The typography system will use system fonts as fallback
-  // Remove this once custom fonts are added and uncomment above
-  return { fontsLoaded: true, fontError: null };
+  return { fontsLoaded, fontError };
 };
 
